502934b79a93fd71467b91b958caa076a9304441
Mukul Rai <mukurai2004@gmail.com>
2025-04-02 11:10:11 +0530

    fix -- enable stock transfer functionality in stock routes

--- src/routes/stock.ts
+++ src/routes/stock.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get("/get_all_stocks", getAllStocks);
 router.post("/add_new_stock", addNewStocks);
-//router.post("/transfer_stock", transferStock);
 router.put("/update_stock", updateStock);
+router.put("/transfer_stock", transferStock);
 
 export default router;
