c4d43bd49216789863c27c2ff1a2bfd2d6b48f26
Mukul Rai <mukurai2004@gmail.com>
2025-04-17 11:04:38 +0530

    fix -- improve formatting and readability in generateBillHistory and getAllBillHistory functions

--- src/controller/billhistory.ts
+++ src/controller/billhistory.ts
@@ -4,7 +4,10 @@ import * as e from "express";
 
 const prisma = new PrismaClient();
 
-export const generateBillHistory = async (req: Request, res: Response): Promise<void> => {
+export const generateBillHistory = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   const {
     stockData,
     date,
@@ -23,25 +26,22 @@ export const generateBillHistory = async (req: Request, res: Response): Promise<
   } = req.body;
 
   try {
-
-
-    const stockUpdate = stockData.map(async (stock:any)=>{
-     const result = await  prisma.stock.update({
-        where:{
-          shop:shopName,
-          id:stock.id,
-          product:stock.product
+    const stockUpdate = stockData.map(async (stock: any) => {
+      const result = await prisma.stock.update({
+        where: {
+          shop: shopName,
+          id: stock.id,
+          product: stock.product,
         },
-        data:{
-          quantity:stock.lastQuantity
-        }
-      })
-    })
-
+        data: {
+          quantity: stock.lastQuantity,
+        },
+      });
+    });
 
     const result = await prisma.billHistory.create({
       data: {
-        updatedStocks: {create:[...stockData]},
+        updatedStocks: { create: [...stockData] },
         pdfDate: date,
         totalSale: totalCash,
         upiPayment: upiPayment,
@@ -57,29 +57,34 @@ export const generateBillHistory = async (req: Request, res: Response): Promise<
         totalCashReceived: cashLeft,
       },
     });
-     res.status(200).json(result);
+    res.status(200).json(result);
   } catch (error) {
     console.log(error);
-    
-     res.status(500).json(error);
+
+    res.status(500).json(error);
   }
 };
 
+export const getAllBillHistory = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { Shop } = req.query;
 
-export const getAllBillHistory = async (req: Request, res: Response): Promise<void> => {
-  const {Shop} = req.query
-  
   try {
     const result = await prisma.billHistory.findMany({
-      where:{
-        shop: Shop as Shop 
-      }
+      where: {
+        shop: Shop as Shop,
+      },
+      orderBy: {
+        pdfDate: "desc",
+      },
     });
-    
-     res.status(200).json(result);
+
+    res.status(200).json(result);
   } catch (error) {
     console.log(error);
-    
-     res.status(500).json(error);
+
+    res.status(500).json(error);
   }
 };
